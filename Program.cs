1b9644305d627d9446f8e39415f11f80ed806a27
Djessika13 <vamp13@gmail.com>
2022-10-13 20:56:40 +0300

    выборка по критериям

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//чтение массива данных
+ string[] M;
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+M = stroka.Split(' ');
+var result = new string[M .Length];
+var realSize = 0;
+//выборка по критерию
+foreach (var value in M )
+    {
+        if (value.Length <= 3)
+        {
+        result[realSize] = value;
+        realSize++;
+        }
+    }
